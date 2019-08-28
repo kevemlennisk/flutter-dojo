141ad7548e2d5d7757104903e778e2e43db5ef29
Kevem Lennisk Almeida Andrade <keven.almeida.andrade@gmail.com>
2019-08-28 20:16:08 -0300

    adds repositories page

--- lib/main.dart
+++ lib/main.dart
@@ -9,18 +9,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home x Page'),
+      initialRoute: '/',
+      routes: {
+        '/' : (context) => MyHomePage(),
+        RepositoriesPage.kRouteName : (context) => RepositoriesPage()
+      },
     );
   }
 }
@@ -48,9 +43,7 @@ class _MyHomePageState extends State<MyHomePage> {
   String _userName;
 
   void _goToUserRepositoryPage() {
-    setState(() {
-
-    });
+      Navigator.pushNamed(context, RepositoriesPage.kRouteName);
   }
 
   @override
@@ -65,7 +58,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Login"),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -106,4 +99,23 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class RepositoriesPage extends StatelessWidget {
+
+  static const kRouteName = '/repositories';
+  static const kPageName = 'Respositories';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(kPageName)
+      ),
+      body: Center(
+        child: Text("")
+      )
+    );
+  }
+  
 }
