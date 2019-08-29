8c55f9a1f0c657a50a9794a6289ff271a9141188
Kevem Andrade <Kevem_Andrade@gap.com>
2019-08-29 00:24:47 -0300

    extrats repository page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/main.dart';
+import 'package:my_app/repositories_page.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.title}) : super(key: key);
---
+++ lib/repositories_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/main.dart';
+
+class RepositoriesPage extends StatelessWidget {
+  static const kRouteName = '/repositories';
+  static const kPageName = 'Respositories';
+
+  List<Repository> _repositories;
+
+  @override
+  Widget build(BuildContext context) {
+    void _goToDetailsPage() {
+      Navigator.pushNamed(context, RepositoryDetail.kRouteName);
+    }
+
+    final String userName = ModalRoute.of(context).settings.arguments;
+    _repositories = mockedRepositories();
+    return Scaffold(
+        appBar: AppBar(title: Text(kPageName)),
+        body: Center(
+            child: Column(
+          children: <Widget>[
+            SizedBox(height: 20),
+            ClipOval(
+              child: Image.network(
+                'https://avatars.githubusercontent.com/$userName',
+                height: 130,
+                width: 130,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(userName),
+            Expanded(
+                child: ListView.builder(
+                    itemCount: _repositories.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                          title: Text('${_repositories[index]._name}'),
+                          onTap: _goToDetailsPage);
+                    }))
+          ],
+        )));
+  }
+
+  List<Repository> mockedRepositories() {
+    List<Repository> repositories = List();
+    repositories.add(Repository('repository 1'));
+    repositories.add(Repository('repository 2'));
+    repositories.add(Repository('repository 3'));
+    repositories.add(Repository('repository 4'));
+    repositories.add(Repository('repository 5'));
+    repositories.add(Repository('repository 5'));
+    repositories.add(Repository('repository 5'));
+    repositories.add(Repository('repository 5'));
+    repositories.add(Repository('repository 5'));
+    repositories.add(Repository('repository 5'));
+    repositories.add(Repository('repository 5'));
+    repositories.add(Repository('repository 5'));
+    return repositories;
+  }
+}
+
+class Repository {
+  String _name;
+
+  Repository(this._name);
+
+  String get name => _name;
+}
