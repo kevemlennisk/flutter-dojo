bf8590d2fbaa1b6109d65a246300c9060c79d83b
Kevem Lennisk Almeida Andrade <keven.almeida.andrade@gmail.com>
2019-08-28 21:05:22 -0300

    adds mocked repositories list

--- lib/main.dart
+++ lib/main.dart
@@ -101,9 +101,12 @@ class RepositoriesPage extends StatelessWidget {
   static const kRouteName = '/repositories';
   static const kPageName = 'Respositories';
 
+  List<Repository> _repositories;
+
   @override
   Widget build(BuildContext context) {
   final String userName = ModalRoute.of(context).settings.arguments;
+  _repositories = mockedRepositories();
     return Scaffold(
         appBar: AppBar(title: Text(kPageName)),
         body: Center(
@@ -115,8 +118,44 @@ class RepositoriesPage extends StatelessWidget {
                   'https://avatars.githubusercontent.com/$userName', height: 130, width: 130,),
             ),
             SizedBox(height: 10),
-            Text(userName)
+            Text(userName),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: _repositories.length,
+                  itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('${_repositories[index]._name}'),
+                  );
+              })
+            )
           ],
         )));
   }
+
+  List<Repository> mockedRepositories() {
+     List<Repository> repositories = List();
+    repositories.add(Repository('repository 1'));
+    repositories.add(Repository('repository 2'));
+    repositories.add(Repository('repository 3'));
+    repositories.add(Repository('repository 4'));
+    repositories.add(Repository('repository 5'));
+    repositories.add(Repository('repository 5'));
+    repositories.add(Repository('repository 5'));
+    repositories.add(Repository('repository 5'));
+    repositories.add(Repository('repository 5'));
+    repositories.add(Repository('repository 5'));
+    repositories.add(Repository('repository 5'));
+    repositories.add(Repository('repository 5'));
+    return repositories;
+  }
+}
+
+class Repository {
+  
+  String _name;
+  
+  Repository(this._name);
+
+  String get name => _name;
+
 }
